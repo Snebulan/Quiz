97bb38167fec8e046b7d4fdf9cf35c148cae67ed
Kaj Allehamn <xp-q@outlook.com>
2018-11-23 08:21:39 +0100

    Add pictureBoxes instead of buttons.

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -27,6 +27,7 @@ namespace Quiz.Model
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new AdminScreen());
             Application.Run(new Default());
+            Application.Run(new QuizScreen());
         }
     }
 }
